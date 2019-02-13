2fd29af0458d9710e181d084ba31e611890bb61f
Ujjwal Gupta <ujjwal@quaqua.com>
2019-02-13 16:02:30 +0530

    httpBasic inMemory authentication

---
+++ security/src/main/java/formBased/FormBasedSpringSecurityApplication.java
@@ -0,0 +1,12 @@
+package formBased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormBasedSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormBasedSpringSecurityApplication.class,args);
+    }
+}
